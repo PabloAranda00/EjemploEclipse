7780afebac5238c0c30c3b36ccc5657a2351202b
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-24 19:01:52 +0100

    imprimo mi nombre y apellido

---
+++ viernes/src/viernes1/viernes1.java
@@ -0,0 +1,14 @@
+package viernes1;
+
+public class viernes1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Pablo");
+		System.out.println("Aranda");
+	}
+
+}
